738244e4e9d09b5e57b1807bce5367dbc308a612
CharlotteGithub-prog <112245678+CharlotteGithub-prog@users.noreply.github.com>
2025-06-27 00:09:12 +0100

    feat: run final EDA following elevation preprocessing

--- src/data_ingestion/static_data_ingestion.py
+++ src/data_ingestion/static_data_ingestion.py
@@ -9,7 +9,6 @@ import numpy as np
 import pandas as pd
 import geopandas as gpd
 from rasterio.merge import merge
-from shapely.geometry import Point
 from rasterstats import zonal_stats
 
 from src.data_ingestion.spatial_transformations import easting_northing_to_lat_long, \
@@ -28,6 +27,7 @@ logger = logging.getLogger(__name__)
 
 # TODO: Make resolution dynamic for land cover
 
+# Land Cover
 def load_land_cover_data(tif_path: str, csv_path: str, catchment: str, shape_filepath: str):
     """
     Loads land cover data from GeoTIFF file using xarray, flattens it to a DataFrame,
@@ -45,7 +45,7 @@ def load_land_cover_data(tif_path: str, csv_path: str, catchment: str, shape_fil
     land_cover_ds = land_cover_ds.squeeze()
     
     # --- Trim to catchment geometry or bounding box ---
-    catchment_gdf, _, minx, miny, maxx, maxy = find_catchment_boundary(
+    _, _, minx, miny, maxx, maxy = find_catchment_boundary(
         catchment=catchment,
         shape_filepath=shape_filepath,
         required_crs=27700
@@ -79,8 +79,9 @@ def load_land_cover_data(tif_path: str, csv_path: str, catchment: str, shape_fil
     agg_land_cover_df.to_csv(csv_path, index=False)
     logger.info(f"Land Cover data succesfully saved to {csv_path}.")
     
-    return agg_land_cover_df, catchment_gdf
+    return agg_land_cover_df
 
+# Land Cover
 def aggregate_land_cover_categories(land_use_df: pd.DataFrame):
     """
     Map land use categories from full detail to primary categories to reduce dimensionality
@@ -100,14 +101,159 @@ def aggregate_land_cover_categories(land_use_df: pd.DataFrame):
         
     return land_use_df
 
-def load_elevation_data(dir_path: str, csv_path: str, catchment: str, shape_filepath: str,
-                        catchment_gdf: gpd.GeoDataFrame, mesh_nodes_gdf: gpd.GeoDataFrame,
+# Elevation
+def load_mosaic_elevation(dir_path: str, mesh_cells_gdf_polygons: gpd.GeoDataFrame, csv_path: str):
+
+    logger.info(f"Loading Elevation Data: Starting DTM processing from {dir_path} directory...")
+    
+    # Find all DTM .asc files recursively
+    dtm_tiles_paths = glob.glob(os.path.join(dir_path, '**', '*.asc'), recursive=True)
+
+    # If no DTM data then log error, add a column of NaNs and return the original mesh GDF
+    if not dtm_tiles_paths:
+        logger.error(f"No .asc files found in {dir_path}. Please check the path and file extensions.")
+        mesh_cells_gdf_polygons['mean_elevation'] = np.nan
+        return mesh_cells_gdf_polygons
+
+    logger.info(f"Found {len(dtm_tiles_paths)} DTM .asc tiles.")
+
+    # --- Mosaic (Merge) the DTM Tiles into a single GeoTIFF using rasterio ---
+    
+    dtm_files_to_mosaic = []
+    
+    try:
+        for filepath in dtm_tiles_paths:
+            data = rasterio.open(filepath)
+            dtm_files_to_mosaic.append(data)
+        
+        mosaic, out_transform = merge(dtm_files_to_mosaic)
+        out_meta = dtm_files_to_mosaic[0].meta.copy()
+        
+        out_meta.update({
+            "driver": "GTiff",
+            "height": mosaic.shape[1],
+            "width": mosaic.shape[2],
+            "transform": out_transform,
+            "crs": dtm_files_to_mosaic[0].crs,
+            "nodata": dtm_files_to_mosaic[0].nodata
+        })
+
+        with rasterio.open(csv_path, "w", **out_meta) as dest:
+            dest.write(mosaic)
+
+        logger.info(f"Merged DTM saved to {csv_path}")
+    
+    except Exception as e:
+        logger.error(f"Error merging DTM tiles: {e}. Cannot proceed with elevation data.", exc_info=True)
+        mesh_cells_gdf_polygons['mean_elevation'] = np.nan
+        return mesh_cells_gdf_polygons
+    
+    finally:
+        # Close connection to file on disk
+        for data in dtm_files_to_mosaic:
+            data.close()
+
+# Elevation
+def calculate_polygon_zone_average(mesh_cells_gdf_polygons: gpd.GeoDataFrame, clipped_dtm: gpd.GeoDataFrame):
+    """
+    Perform Zonal Statistics (rasterstats) to calculate mean elevation for each 1km mesh polygon.
+    
+    Return
+        mesh_cells_gdf_polygons (pd.GeoDataFrame): 1km average elevation statistics in catchment
+    """
+    if mesh_cells_gdf_polygons.geometry.iloc[0].geom_type != 'Polygon':
+        # If wrong data type log error and return unaggregated catchment data
+        logger.error("mesh_cells_gdf_polygons geometries are not polygons. Zonal statistics requires polygons.")
+        mesh_cells_gdf_polygons['mean_elevation'] = np.nan
+        return mesh_cells_gdf_polygons
+    
+    if str(mesh_cells_gdf_polygons.crs) != str(clipped_dtm.rio.crs):
+        logger.warning(f"Mesh cells GeoDataFrame CRS ({mesh_cells_gdf_polygons.crs}) does not match clipped "
+                        f"DTM CRS ({clipped_dtm.rio.crs}). Reprojecting mesh cells for zonal stats.")
+        mesh_cells_gdf_polygons = mesh_cells_gdf_polygons.to_crs(clipped_dtm.rio.crs)
+
+    stats = zonal_stats(
+        mesh_cells_gdf_polygons,
+        clipped_dtm.values,
+        affine=clipped_dtm.rio.transform(),
+        stats="mean",
+        nodata=clipped_dtm.rio.nodata
+    )
+
+    mesh_cells_gdf_polygons['mean_elevation'] = [s['mean'] if s and 'mean' in s else np.nan for s in stats]
+    logger.info("Mean elevation calculated for each mesh node using zonal statistics.")
+    
+    return mesh_cells_gdf_polygons
+
+# Elevation
+def preprocess_elevation_data(mesh_cells_gdf_polygons: gpd.GeoDataFrame, elev_max: float, elev_min: float,
+                              catchment: str):
+    """
+    Ensure there is no outlying data and all sit within known catchment bounds
+    """
+    # Define bounds
+    logging.info(f"Check {catchment} catchment elevation sits in known bounds: "
+                 f"{elev_min} mAOD - {elev_max} mAOD...")
+    
+    # Count if any averages sit outside bounds
+    capped_lower_count = (mesh_cells_gdf_polygons['mean_elevation'] < elev_min).sum()
+    capped_upper_count = (mesh_cells_gdf_polygons['mean_elevation'] > elev_max).sum()
+    
+    # Cap to catchment bounds
+    if capped_lower_count > 0 or capped_upper_count > 0:
+        logger.warning(f"WARNING: Capping {capped_lower_count} nodes below {elev_min:.2f} mAOD and "
+                       f"{capped_upper_count} nodes above {elev_max:.2f} mAOD.")
+        
+        mesh_cells_gdf_polygons['mean_elevation'] = np.clip(
+            mesh_cells_gdf_polygons['mean_elevation'],
+            a_min=elev_min,
+            a_max=elev_max
+        )
+        
+    else:
+        logger.info(f"All 'mean_elevation' values within known bounds ({elev_min:.2f} - {elev_max:.2f} mAOD). No capping performed.\n")
+        
+    # Check for NaNs
+    NaN_count = mesh_cells_gdf_polygons['mean_elevation'].isna().sum()
+    if NaN_count > 0:
+        logging.warning(f"WARNING: {NaN_count} nodes with NaN value.")
+    
+    return mesh_cells_gdf_polygons
+
+# Elevation
+def load_process_elevation_data(dir_path: str, csv_path: str, catchment: str, catchment_gdf: gpd.GeoDataFrame,
+                        mesh_cells_gdf_polygons: gpd.GeoDataFrame, elev_max: float, elev_min: float,
                         grid_resolution: int = 1000):
     """
     Loads OS elevation DTM data from tile directory using rasterio, flattens it to a DataFrame,
     converts x, y coordinates to lat/lon, and saves to CSV.
     """
     
+    # Mosaic (Merge) the DTM Tiles into a single GeoTIFF using rasterio
+    load_mosaic_elevation(dir_path, mesh_cells_gdf_polygons, csv_path)
+
+    # Load merged DTM
+    dtm_raster = rioxarray.open_rasterio(csv_path, masked=True).squeeze()
+
+    # Reproject crs if needed
+    if str(dtm_raster.rio.crs) != str(catchment_gdf.crs):
+        dtm_raster = dtm_raster.rio.reproject(catchment_gdf.crs)
+
+    # Clip to bounding box using rio.clip_box()
+    minx, miny, maxx, maxy = catchment_gdf.total_bounds
+    clipped_dtm = dtm_raster.rio.clip_box(minx, miny, maxx, maxy)
+    logger.info("DTM raster clipped to catchment bounding box.")
+    
+    # Aggregate polygon data to averages using rasterstats lib  
+    mesh_cells_gdf_polygons = calculate_polygon_zone_average(mesh_cells_gdf_polygons, clipped_dtm)
+    logger.info(f"Loading, merging and aggregating DTM elevation data complete "
+                f"for {catchment} catchment.\n")
+    
+    # Check for outliers and inconsistencies
+    mesh_cells_gdf_polygons = preprocess_elevation_data(mesh_cells_gdf_polygons, elev_max,
+                                                        elev_min, catchment)
+    
+    return mesh_cells_gdf_polygons
     
 def load_slope_data():
     pass
