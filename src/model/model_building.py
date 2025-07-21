e6d8049ce471c73b0263204e9e5e2f6d8d6db105
Charlotte Wayment <112245678+CharlotteGithub-prog@users.noreply.github.com>
2025-07-21 12:50:45 +0100

    init: instantiate model building src files and notebooks for main model dev

---
+++ src/model/model_building.py
@@ -0,0 +1,22 @@
+# Import Libraries
+import os
+import sys
+import torch
+import logging
+import datetime
+import numpy as np
+import pandas as pd
+from torch_geometric.data import Data
+    
+# Set up logger config
+logging.basicConfig(
+    level=logging.INFO,
+   format='%(levelname)s - %(message)s',
+#    format='%(asctime)s - %(levelname)s - %(name)s - %(message)s',
+    handlers=[logging.StreamHandler(sys.stdout)]
+)
+
+# Set up logger for file and load config file for paths and params
+logger = logging.getLogger(__name__)
+
+# Apply data loaders
