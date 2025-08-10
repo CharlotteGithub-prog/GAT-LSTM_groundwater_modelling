3d07e12e8cadf0100e3351dfcc2dd21b0cbc960e
Charlotte Wayment <112245678+CharlotteGithub-prog@users.noreply.github.com>
2025-08-10 20:43:08 +0100

    feat: setup slurm pipelines for main model and training pipelines

---
+++ slurm/training_pipeline.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# Instructing SLURM to locate and assign X number of nodes with Y number of cores in each node.
+# X,Y are integers. Refer to table for various combinations. X will almost always be 1.
+#SBATCH -N 1
+#SBATCH -c 1
+
+# Governs the run time limit and resource limit for the job. Pick values from the partition and QOS tables.
+#SBATCH --gres=gpu:pascal:1
+#SBATCH -p tpg-gpu-small
+#SBATCH --qos=short
+#SBATCH -t 01-00:00:00
+#SBATCH --mem=20G
+
+#SBATCH --job-name=GWL_pipeline_run
+#SBATCH -o /home3/swlc12/msc-groundwater-gwl/slurm/logs/slurm-%j.out
+
+#SBATCH --mail-type=FAIL
+#SBATCH --mail-user=swlc12@durham.ac.uk
+
+# Faily immediately with errors, undefined vars or if any command fails (not just last)
+set -euo pipefail
+
+# Source the bash profile (required to use the module command, ignore warning blocks here)
+set +u
+source /etc/profile
+set -u
+
+# Activate .venv
+source /home3/swlc12/msc-groundwater-gwl/.venv_ncc/bin/activate
+
+# Log selected GPU and node before runnign code
+echo "Running on $(hostname)"
+nvidia-smi || true
+
+# Go to project dir (helps with relative paths/checkpoints)
+cd /home3/swlc12/msc-groundwater-gwl
+
+# Run your program (replace this with your program)
+python main_training.py
